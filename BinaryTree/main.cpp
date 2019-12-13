952f5eb54c93cc30831aabf5af88a36e5f12f0ec
Adeeb Mohammed <adeeb2358@gmail.com>
2019-12-13 09:43:19 +0900

    branch sum solution

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -7,10 +7,43 @@
 //
 
 #include <iostream>
+#include <vector>
+// for the sake of compilation for this logic adding a binary tree
+class BinaryTree{
+    public :
+    int value;
+    BinaryTree *left;
+    BinaryTree *right;
+    
+    // binary search tree constructor
+    BinaryTree(int value){
+        this->value = value;
+        this->left  = NULL;
+        this->right = NULL;
+    }
+};
 
+// function for calculating the branch sum of a binary tree
+void calculateBranchSums(BinaryTree *tree, int runningSum ,std::vector<int> &sums){
+    if(tree == NULL){
+        return;
+    }
+    if(tree->left == NULL && tree->right == NULL){
+        sums.push_back(runningSum+tree->value);
+    }
+    calculateBranchSums(tree->left, runningSum+tree->value,sums);
+    calculateBranchSums(tree->right,runningSum+tree->value,sums);
+    return ;
+}
+
+std::vector<int> branchSum(BinaryTree *tree){
+    std::vector<int> sums;
+    calculateBranchSums(tree,0,sums);
+    return sums;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "function for branch sum";
     return 0;
 }
